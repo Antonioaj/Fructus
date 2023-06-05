b7c552a79698efcc36f2bbdc7e67e1dec3062dc8
AntonioAj <antonioaj@hotmail.co.uk>
2023-06-05 14:52:52 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Antonio Ajewole on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
